1806ef10a26b5b85adaa7cc2a3075f5fe62a7cdf
Nusrat <nusratilla.88@gmail.com>
2023-12-01 01:09:37 +0500

    a

---
+++ src/components/app/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>Hello World <i class="bi bi-heart"></i></div>
+  <AppInfo />
+  <div>Hello World <i class="bi bi-heart-fill"></i></div>
+</template>
+
+<script>
+import AppInfo from "@/components/app-info/AppInfo.vue"
+export default {
+  components: {
+    AppInfo
+  }
+}
+</script>
+
+<style></style>
