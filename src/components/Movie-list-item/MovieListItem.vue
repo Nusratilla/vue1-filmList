fbbbdd49dd1b64bb3dafa5c8ffe7333a14b34e23
Nusrat <nusratilla.88@gmail.com>
2023-12-05 23:16:48 +0500

    a

--- src/components/Movie-list-item/MovieListItem.vue
+++ src/components/Movie-list-item/MovieListItem.vue
@@ -2,7 +2,7 @@
 <template >
   <li class="list-group-item d-flex justify-content-between"
     :class="[{ like: movie.like }, { favourite: movie.favourite }]">
-    <span class="list-group-item-label">{{ movie.name }}</span>
+    <span @click="onLike" class="list-group-item-label">{{ movie.name }}</span>
     <input type="number" class="list-group-item-input" v-bind:value="movie.viewers">
     <div class="d-flex justify-content-center align-items-center">
       <button type="button" class="btn-cookie btn-sm">
@@ -24,7 +24,12 @@ export default {
       type: Object,
       required: true,
     }
-  }
+  },
+  methods: {
+    onLike() {
+      this.$emit('onLike', this.movie.id)
+    }
+  },
 }
 </script>
 <style scoped>
